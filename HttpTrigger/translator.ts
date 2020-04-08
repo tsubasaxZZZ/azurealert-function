ec20f4b3e4926405d279caf02c60ffdbaec33713
tsunomur <tsunomur@microsoft.com>
2020-04-08 12:32:49 +0900

    Change uudiv4 -> uuid. uuidv4 is deprecated.

--- HttpTrigger/translator.ts
+++ HttpTrigger/translator.ts
@@ -1,4 +1,4 @@
-import * as uuidv4 from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 import axios, { AxiosInstance } from 'axios';
 import { env } from "process"
 import * as dotenv from "dotenv"
